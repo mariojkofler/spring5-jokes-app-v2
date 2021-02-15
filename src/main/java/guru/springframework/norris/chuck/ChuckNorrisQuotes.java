189533a2ddb36ec8b1b619fad172e39e50fdb8b0
Mario J. Kofler <mario.j.kofler@runbox.com>
2021-02-15 08:55:36 +0100

    Created service. closes #3.

--- src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.norris.chuck;
-
-/**
- * Created by mjk on 15.02.21
- */
-
-public class ChuckNorrisQuotes {
-}
--- src/main/java/guru/springframework/spring5jokesappv2/Spring5jokesappApplication.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5jokesappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.norris;
+package guru.springframework.spring5jokesappv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
