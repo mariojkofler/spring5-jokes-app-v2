bf053e76be8c19455690c79d663edd32a8b00071
Mario J. Kofler <mario.j.kofler@runbox.com>
2021-02-15 08:29:44 +0100

    Created project. closes #1.

---
+++ src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes.java
@@ -0,0 +1,8 @@
+package guru.springframework.norris.chuck;
+
+/**
+ * Created by mjk on 15.02.21
+ */
+
+public class ChuckNorrisQuotes {
+}
---
+++ src/test/java/guru/springframework/norris/Spring5jokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.norris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5jokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
